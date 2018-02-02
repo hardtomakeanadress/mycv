dc97bb9b2e46ed7af905b29d31057a840078142c
StefanB <hardtomakeanadress@hotmail.com>
2018-02-02 11:20:07 +0200

    add github reference to SocialMedia; edit css rules for <a>

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -4,20 +4,21 @@ import './SocialMedia.css';
 import mailLogo from './img/email.png';
 import phoneLogo from './img/phone.png';
 import linkedin from './img/linkedin.png';
+import github from './img/github.png';
 
 const SocialMedia = (props) => {
     return(
         <div id='SocialMedia'>
             <div className="row">
-                <div className="col-sm-4">
+                <div className="col-sm-3">
                     <img  src={phoneLogo} alt="contact me by phone"/>
                     <span>+40 728 005 406</span>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-sm-3">
                     <img  src={mailLogo} alt="contact me by mail"/>
                     <span>adi.stef@gmx.com</span>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-sm-3">
                     <a href="https://www.linkedin.com/in/adistef/" 
                        target="_blank"
                        rel="noopener noreferrer">
@@ -27,6 +28,18 @@ const SocialMedia = (props) => {
                        target="_blank"
                        rel="noopener noreferrer">
                         <span>My Linkedin Profile</span>
+                    </a>
+                </div>
+                <div className="col-sm-3">
+                    <a href="https://github.com/hardtomakeanadress" 
+                       target="_blank"
+                       rel="noopener noreferrer">
+                        <img src={github} alt="Check my GitHub"/>
+                    </a>
+                    <a href="https://github.com/hardtomakeanadress" 
+                       target="_blank"
+                       rel="noopener noreferrer">
+                        <span>My GitHub</span>
                     </a>
                 </div>
             </div>
